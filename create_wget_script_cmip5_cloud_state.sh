06668fae62704d354e50f0c6ae192c97201e4559
Phil Austin <paustin@eos.ubc.ca>
2014-11-11 12:50:18 -0800

    try historical

--- create_wget_script_cmip5_cloud_state.sh
+++ create_wget_script_cmip5_cloud_state.sh
@@ -13,14 +13,16 @@
 # tasmin
 # tasmax
 
-vartype="cloud_state"
-variables="variable=clwvi&variable=clivi&variable=clt&variable=snc&variable=tas&variable=tasmin&variable=tasmax"
+vartype="historical_sfc_temp"
+variables="variable=ts"
 #
-for model in GFDL-CM3 BCC-CSM1.1 MRI-CGCM3 MPI-ESM-LR CNRM-CM5 IPSL-CM5B-LR IPSL-CM5A-LR IPSL-CM5A-MR MIROC5 CanAM4 HadGEM2-A
+#for model in GFDL-CM3 BCC-CSM1.1 MRI-CGCM3 MPI-ESM-LR CNRM-CM5 IPSL-CM5B-LR IPSL-CM5A-LR IPSL-CM5A-MR MIROC5 CanAM4 HadGEM2-A
+
+for model in HadGEM2-ES
 
 do
  mkdir -p ${model}/${vartype}
- curl -o ${model}/${vartype}/wget_${model}_${vartype}.sh "http://pcmdi9.llnl.gov/esg-search/wget/?${variables}&model=${model}&project=CMIP5&experiment=amip&cmor_table=Amon&cmor_table=LImon&ensemble=r1i1p1&distrib=true"
+ curl -o ${model}/${vartype}/wget_${model}_${vartype}.sh "http://pcmdi9.llnl.gov/esg-search/wget/?${variables}&model=${model}&project=CMIP5&experiment=historical&cmor_table=Amon&cmor_table=LImon&ensemble=r1i1p1&distrib=true"
  chmod a+rx ${model}/${vartype}/wget_${model}_${vartype}.sh
 
 
